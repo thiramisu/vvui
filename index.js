e09271e70396248a0fa190f369c3196ea5bf962c
tiramisu_oTATo <xxx.partyo@gmail.com>
2023-08-09 23:49:25 +0900

    タイトルメニュー表示までのアニメーション完成

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 
+const asleep = (duration) => new Promise((resolve) => {
+  setInterval(resolve, duration);
+});
 let lastPointerEventDispachTime = 0;
 const CALC_INTERVAL = 1000;
+const BACKGROUND_ID_MAX = 7;
 
 const sinInOut = (zeroToOne) => Math.sin((zeroToOne - 0.5) * Math.PI) + 0.5;
 
@@ -13,15 +17,20 @@ window.addEventListener("load", () => {
   };
   const titleScreen = document.getElementById("title-screen");
   const titleBg = document.getElementById("title-bg");
-  const titleBgSize = (() => {
-    const { width, height } = titleBg.getBoundingClientRect();
-    return { width, height };
-  })();
+  titleBg.src = `top-illust-${Math.floor(Math.random() * BACKGROUND_ID_MAX + 1).toString().padStart(3, '0')}.webp`;
+  let titleBgSize;
   let titleClientRect = titleScreen.getBoundingClientRect();
+  const titleLogo = document.getElementById("title-logo");
   
   const changeTitleImagePosition = (event) => {
     const now = Date.now();
     if (now - lastPointerEventDispachTime > CALC_INTERVAL) {
+      if (titleBgSize === undefined || titleBgSize.width === 0) {
+        titleBgSize = (() => {
+          const { width, height } = titleBg.getBoundingClientRect();
+          return { width, height };
+        })();
+      }
       pageSize = {
         width: page.scrollWidth,
         height: page.scrollHeight,
@@ -39,4 +48,26 @@ window.addEventListener("load", () => {
   }
 
   page.addEventListener("pointermove", changeTitleImagePosition);
+  animation(titleLogo);
 });
+
+const animation = async (titleLogo) => {
+  const titleMonoBg = document.getElementById("title-mono-bg");
+  const titlePrimaryBg = document.getElementById("title-primary-bg");
+  const titleLogoShadow = document.getElementById("title-logo-shadow");
+  const titleDescription = document.getElementById("title-description");
+  
+  titleLogo.classList.add("title-logo-transform-animation1");
+  await asleep(1250);
+  titleLogo.classList.add("title-logo-fill-animation1");
+  await asleep(1000);
+  titleLogo.classList.add("title-logo-fill-animation2");
+  titleMonoBg.classList.add("to-transparent");
+  titleLogoShadow.classList.add("to-transparent");
+  await asleep(750);
+  titleDescription.classList.add("title-description-animation");
+  titleLogo.classList.add("title-logo-transform-animation2");
+  await asleep(2000);
+  titlePrimaryBg.classList.add("to-transparent");
+  titleLogo.classList.add("title-logo-fill-animation3");
+};
